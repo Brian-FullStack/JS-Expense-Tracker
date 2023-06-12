a92b3cb23f676ef9b2aa22eb1b2e627ff8e26e8a
Brian-FullStack <brian.fullstack@gmail.com>
2023-06-12 16:02:26 +0100

    Added empty functions with a starting plan fo attack (POA).

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+const addTransactionToHistory = (activeIndicator, transactionArray) => {
+    /**Use the .unshift() method to add new transaction to transaction array
+     * if activeIndicator == true set activeIndicator.classList to green
+     * use the .pop() method to remove the last transaction if the transactionArray.length > 5
+     */
+}
+
+const updateBalanceSheet = (income, expense, balance) => {
+    /**if the newTransaction.amount > 0 {
+     * ADD newTransaction.amount to income AND balance
+     * } else {
+     * ADD newTransaction.amount to expenses and SUBTRACT from balance} */
+
+}
+
+const createNewTransaction = () => {
+    /**Create a new object using the transaction.Name and transaction.Amount
+     * push() the new Object to the transactionArray
+     * if amount is less than 0 activeIndicator == flase
+     * if amount is more than 0 activeIndicator == true
+     */
+}
+
+/**Clcik event for add button */
