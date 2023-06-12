9bcbecd182f5e883206ccfadd4dc0028ab49834d
Brian-FullStack <brian.fullstack@gmail.com>
2023-06-12 19:35:19 +0100

    Added functionality to createNewTransaction. Updated css classes

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+const addBtn = document.querySelector("#addBtn");
+let activeIndicator = document.querySelector(".activeIndicator");
+const transactionArray = [];
 
 
-const addTransactionToHistory = (activeIndicator, transactionArray) => {
+
+const addTransactionToHistory = (transaction) => {
     /**Use the .unshift() method to add new transaction to transaction array
      * if activeIndicator == true set activeIndicator.classList to green
      * use the .pop() method to remove the last transaction if the transactionArray.length > 5
      */
+
 }
 
 const updateBalanceSheet = (income, expense, balance) => {
@@ -21,6 +26,27 @@ const createNewTransaction = () => {
      * if amount is less than 0 activeIndicator == flase
      * if amount is more than 0 activeIndicator == true
      */
+    let name = document.querySelector("#name").value;
+    let amount = document.querySelector("#amount").value;
+    let asset = false;
+    
+    if (amount >= 0) {
+        activeIndicator.classList.add("activeIndicatorGreen");
+        asset = true
+    } else {
+        activeIndicator.classList.add("activeIndicatorRed");
+    }
+
+    let newTransaction = {
+        name: name.toLowerCase(),
+        amount: amount,
+        asset: asset
+    };
+
+    console.log(newTransaction)
 }
 
 /**Clcik event for add button */
+addBtn.addEventListener("click", () => {
+    createNewTransaction();
+});
